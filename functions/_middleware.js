2d2a1b173329c089a11b8d5c8a018662393482b5
Bünyamin Atik <71401751+haktel@users.noreply.github.com>
2026-09-06 00:57:14 +0200

    Update HOME_CONCEPT_CSS version to 5

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
 const EXCLUDED_PREFIXES=["/admin/","/bais-control-center/","/api/","/assets/","/kundenbereich/","/project-portal/"];
 const BRAND_FAVICON='<link rel="icon" href="/assets/bais-favicon.svg?v=4" type="image/svg+xml">';
 const BRAND_THEME_COLOR='<meta name="theme-color" content="#0B2D45">';
-const HOME_CONCEPT_CSS='<link rel="stylesheet" href="/assets/home-concept2.css?v=4">';
+const HOME_CONCEPT_CSS='<link rel="stylesheet" href="/assets/home-concept2.css?v=5">';
 const HOME_CONCEPT_JS='<script src="/assets/home.js?v=4" defer></script>';
 
 const shouldTrack=pathname=>!EXCLUDED_PREFIXES.some(prefix=>pathname.startsWith(prefix));
